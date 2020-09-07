ba2416349f0e4323b75de5689d5c65732ba81ac4
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-07 01:58:11 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  attr_accessor :name, :breed, :age 
+  
+  @@all = []
+  
+  def initialize 
+    @name = name
+    @breed = breed 
+    @age = age 
+  end 
+  
+  def self.save 
+    @@all << self 
+  end 
